92baa7fee5b0c26f8c5e9a99e3a534b3766ea84b
Sooamazing <91259986+Sooamazing@users.noreply.github.com>
2024-05-21 16:26:41 +0900

    modify profile, projects -before deploy

--- site.config.js
+++ site.config.js
@@ -1,25 +1,26 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "DooMua",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "backend developer",
+    bio: "I develop everything using java.",
+    email: "honang515v@gmail.com",
+    linkedin: "",
+    github: "Sooamazing",
     instagram: "",
   },
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/Sooamazing/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
     title: "morethan-log",
     description: "welcome to morethan-log!",
+    theme: "auto",
   },
 
   // CONFIG configration (required)
